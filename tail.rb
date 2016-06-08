9d9d538400c0a9d6111a048211126ef7742e2ce0
Dmitry Risics <dmitrijs.risics@vce.com>
2016-06-08 18:23:35 +0100

    latest version

--- tail.rb
+++ tail.rb
@@ -3,6 +3,50 @@ require 'docker-api'
 require 'grok-pure'
 require 'pp'
 
+class UpdateError < StandardError; end
+class MissingArgument < StandardError; end
+
+def add(zone, hostname, ip, key, server = nil)
+  raise UpdateError, "can't find SOA for #{zone}" if server.nil?
+  
+  seq  = <<-";"
+            server #{server}
+            key #{zone}. #{key}
+            zone #{zone}.
+            update add #{hostname}.#{zone}. 86400 A #{ip}
+            ;
+    
+  seq += "                send\n"
+
+  invoke_nsupdate(seq)
+end
+
+def delete(zone, hostname, key, server = nil)
+  raise UpdateError, "can't find SOA for #{zone}" if server.nil?
+  
+  seq  = <<-";"
+            server #{server}
+            key #{zone}. #{key}
+            zone #{zone}.
+            update delete #{hostname}.#{zone}. A
+            ;
+    
+  seq += "                send\n"
+
+  invoke_nsupdate(seq)
+end
+
+def invoke_nsupdate(seq = nil)
+  raise MissingArgument, "missing update sequence" if seq.nil?
+
+  Open4.popen4("nsupdate") do |pid, stdin, stdout, stderr|
+    stdin << seq
+    stdin.close_write
+    err = stderr.read
+    raise UpdateError, err unless err.empty?
+  end
+end
+
 grok = Grok.new
 grok.add_patterns_from_file("patterns/base")
 pattern = "%{TIMESTAMP_ISO8601:ts}\s%{NOTSPACE:class}\s%{NOTSPACE:action}\s%{NOTSPACE:id}"
@@ -24,11 +68,28 @@ Open4::popen4 "docker events" do |pid, stdin, stdout, stderr|
 
       case data[:action]
         when 'start'
-          pp Docker::Container.get(data[:id])
-          pp Docker::Container.get(data[:id]).info["NetworkSettings"]["IPAddress"]
-          pp Docker::Container.get(data[:id]).info["Config"]["Hostname"]
+          #pp Docker::Container.get(data[:id])
+          ip = Docker::Container.get(data[:id]).info["NetworkSettings"]["IPAddress"]
+          hostname = Docker::Container.get(data[:id]).info["Config"]["Hostname"]
+          pp ip
+          pp hostname
+
+          add('test.local',
+              hostname, 
+              ip,
+              'h3KXM9Oq9q5iXCfXrnlVNGRj7iUxJPB1b6G91PFZZ8WgYwaB8E0BkpnVDavFU30emC3RVVqiyLeel76NiqeBMg==',
+              '172.17.0.1')
+
         when 'die'
-          pp Docker::Container.get(data[:id]).info["Config"]["Hostname"]
+          #ip = Docker::Container.get(data[:id]).info["NetworkSettings"]["IPAddress"]
+          hostname = Docker::Container.get(data[:id]).info["Config"]["Hostname"]
+          # pp ip
+          pp hostname
+          delete('test.local',
+              hostname, 
+              'h3KXM9Oq9q5iXCfXrnlVNGRj7iUxJPB1b6G91PFZZ8WgYwaB8E0BkpnVDavFU30emC3RVVqiyLeel76NiqeBMg==',
+              '172.17.0.1')
+          # DDNSUpdate.update('test.local', '', 'h3KXM9Oq9q5iXCfXrnlVNGRj7iUxJPB1b6G91PFZZ8WgYwaB8E0BkpnVDavFU30emC3RVVqiyLeel76NiqeBMg==', false, '172.17.0.1')
       end
     end
 
